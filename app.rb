7f74452f3726ad89449e1f4b5220dc087fab2f22
sranso <sranso@gmail.com>
2013-11-23 13:00:17 -0500

    create class App in app.rb that can access information from classes Student and Scraper. update scraper.rb

--- app.rb
+++ app.rb
@@ -1,32 +1,60 @@
 #allows to interface with stuff scraped. 
 
 # require_relative '../lib/*'
+require 'launchy'
 require './lib/scraper.rb'
 require './lib/student.rb'
 
-# 1. make a new scraper
-student_scraper = Scraper.new("http://flatironschool-bk.herokuapp.com/")
+class App
+  attr_reader :names, :blogs, :twitters
 
-# 2. get the student names from scraper
-names = student_scraper.get_students_names
+  def initialize
+    student_scraper = Scraper.new("http://flatironschool-bk.herokuapp.com/")
+    @names = student_scraper.get_students_names #array
+    @blogs = student_scraper.get_students_blogs #array
+    @twitters = student_scraper.get_students_twitter_usernames #array
+  end
+  
+  def generate_class
+    students = []
+    28.times do |i|
+        students << Student.new(names[i], blogs[i], twitters[i]) #instance of class Student, referring to initialized variables (which are arrays)
+    end
+    students #array of student objects
+  end
 
-# 3. get blogs
-blogs = student_scraper.get_students_blogs
+  def puts_class
+    generate_class.each do |classmate|
+      puts "Name: #{classmate.name}, Blog: #{classmate.blog}, Twitter: #{classmate.twitter}"
+    end
+  end
 
-# 4. get twitters
-twitters = student_scraper.get_students_twitter_usernames
-puts twitters
+  def get_rando_blog
+    Launchy.open("#{blogs.sample}")
+  end
 
-# 5. make a new student object for each person
-#this is what we are doing for each: Sarah_Ransohoff = Student.new()
-students = []
-28.times do |i|
-    students << Student.new(names[i], blogs[i], twitters[i])
+  def get_rando_twitter
+    Launchy.open("www.twitter.com/#{twitters.sample[1..-1]}")
+  end
+
+  def get_specific_blog(student_name)
+    generate_class.each do |each_student|
+      if each_student.name.start_with?(student_name)
+        Launchy.open("#{each_student.blog}")
+      end
+    end
+  end
+
+  def get_specific_twitter(student_name)
+    generate_class.each do |each_student|
+      if each_student.name.start_with?(student_name)
+        Launchy.open("www.twitter.com/#{each_student.twitter[1..-1]}")
+      end
+    end
+  end
+
 end
 
-# puts students
+test = App.new
 
-students.each do |person|
-    puts "n #{person.name} b #{person.blog} t #{person.twitter} "
-end
-
+# test.get_specific_twitter("Sarah Ransohoff")
--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,63 +2,67 @@ require 'open-uri' #open the file on the internet
 require 'nokogiri' #line10: Nokogiri is a class, HTML is a method, download is the parameter
 
 class Scraper
-    
-    attr_reader :html
+  
+  attr_reader :html
 
-    def initialize(url)
-        download = open(url)
-        @html = Nokogiri::HTML(download)
-    end
+  def initialize(url)
+      download = open(url)
+      @html = Nokogiri::HTML(download)
+  end
 
-    def get_students_names
-        all_the_h3s = html.search("h3")
-        new_names = all_the_h3s.text.split(/(?<=[a-z.])(?=[A-Z])/)
-    end
+  def get_students_names
+      all_the_h3s = html.search("h3")
+      # new_names = all_the_h3s.text.split(/(?<=[a-z.])(?=[A-Z])/)
+      # all_the_h3s.text --> calls .text on an entire array
+      all_the_h3s.collect do |h3| # --> calls .text on each item in an array
+          h3.text # -->gets the text from each item in array
+      end
+  end
 
-    def get_students_blogs
-        blogs = []
-        # num_of_blogs = html.search("ul .social").length
-        html.search("ul .social").each do |social_div|
-            if social_div.search("a.blog").text == "Blog"
-                blogs << social_div.search("a.blog")[0]["href"]
-            else
-                blogs << "none"
-            end
-        end
-        blogs
-        #==older code
-        # blogs = []
-        # num_of_blogs = html.search("li .blog").length
-        # num_of_blogs.times do |i|
-        #     blogs << html.search("li .blog")[i]["href"]
-        # end
-        # blogs
-    end
+  def get_students_blogs
 
-    def get_students_twitter_usernames
-        # mixed_twitters = html.search("li:first-child a").text.split(" ")
-        # twitter_array = mixed_twitters.select { |name| name[0] = "@" }
-        # ==experimental code
-        twitters = []
-        # num_of_twitters = html.search("li:first-child a").length
-        html.search("li:first-child a").text.split(" ").each do |li_first|
-            if li_first[0] == "@"
-                twitters << li_first
-            else
-                twitters << "none"
-            end
-        end
-        twitters
-    end
+      #==code v3 --> doesn't account for those w/o blogs
+      #html.search("a.blog").collect { |anchor| anchor["href"] }
+      #html.search("a.blog @href").collect { |anchor| anchor.text } --> also works
+      
+      #==code v2 --> accounts for those w/o blogs
+      html.search("ul .back").collect do |back_side| #every ul w/ the class social
+          if back_side.search("a.blog").text == "Blog"
+              back_side.search("a.blog")[0]["href"]
+          else
+              "none"
+          end
+      end
+
+      #==code v1
+      # blogs = []
+      # num_of_blogs = html.search("li .blog").length
+      # num_of_blogs.times do |i|
+      #     blogs << html.search("li .blog")[i]["href"]
+      # end
+      # blogs
+  end
+
+  def get_students_twitter_usernames
+      # mixed_twitters = html.search("li:first-child a").text.split(" ")
+      # twitter_array = mixed_twitters.select { |name| name[0] = "@" }
+      # ==experimental code
+      twitters = []
+      # num_of_twitters = html.search("li:first-child a").length
+      html.search("li:first-child a").text.split(" ").each do |li_first|
+          if li_first[0] == "@"
+              twitters << li_first
+          else
+              twitters << "none"
+          end
+      end
+      twitters
+  end
 end
 
 #these are the things you'll wanna do in your test
 my_scraper = Scraper.new("http://flatironschool-bk.herokuapp.com/")
 # puts my_scraper.get_students_names
-# puts my_scraper.html #show all text
-puts my_scraper.get_students_twitter_usernames #show twitter name
+# puts my_scraper.get_students_twitter_usernames #show twitter name
 # puts my_scraper.get_students_blogs #show blogs
 
-#get blogs-- in a tag and href attribute, get twitter urls
-
-#what should a studnet do: respond to name. 
